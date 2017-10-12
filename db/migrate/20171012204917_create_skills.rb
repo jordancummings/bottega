ea9e5aa430be9922a3974890027f96eb7b3a01b3
Mac <mac@MacBook-Pro.ad.uvu.edu>
2017-10-12 15:43:52 -0600

    Integrated skills via model generator

---
+++ db/migrate/20171012204917_create_skills.rb
@@ -0,0 +1,11 @@
+class CreateSkills < ActiveRecord::Migration[5.1]
+  def change
+    create_table :skills do |t|
+      t.string :title
+      t.integer :percent_utilized
+      t.integer :percent_completed
+
+      t.timestamps
+    end
+  end
+end
