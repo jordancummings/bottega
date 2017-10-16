d43b3aef764a643aaf714533134280c18c60c8fe
Mac <mac@MacBook-Pro.ad.uvu.edu>
2017-10-16 11:39:55 -0600

    Added create and edit buttons to portfolios pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   end
 
   def new
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :body))
+    @portfolio_item = Portfolio.new
   end
 
 
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to @portfolios_path, notice: 'Your post is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your post is now live.' }
       else
           format.html { render :new }
       end
