b233fa20b3229bf44a0b0d8a73fe8bb211006702
Mac <mac@MacBook-Pro.local>
2017-10-17 16:23:29 -0600

    Implemented data defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,15 @@ class Portfolio < ApplicationRecord
 
   # Scopes to call specific subtitles/topics/etc
   # def self.angular
-  #   where(subtitle: "Angular")
+  #   where(subtitle: 'Angular')
   # end
 
-  # scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+  # scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
