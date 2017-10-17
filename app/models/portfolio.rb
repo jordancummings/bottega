f2e64ee9a8ec90c3b8af7347a217e806d1e4a687
Mac <mac@MacBook-Pro.local>
2017-10-17 15:57:02 -0600

    Custom scopes added

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  # Scopes to call specific subtitles/topics/etc
+  # def self.angular
+  #   where(subtitle: "Angular")
+  # end
+
+  # scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
 end
