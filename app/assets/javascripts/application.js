decd571856a892807748c038791990e9270ddd7a
Mac <mac@MacBook-Pro.local>
2017-10-27 18:09:21 -0600

    Implemented new styling for nav bar and masthead

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,19 @@
 
 
 
-$('#navbar li').click(function() {
-    $(this).addClass('active').siblings('li').removeClass('active');
+
+
+
+
+var $menu = $('#menu'), // replace #menu with your selector
+  menuOffsetTop = $menu[0].offsetTop;
+
+$(document).bind('ready scroll', function () {
+  var docScroll = $(this).scrollTop();
+
+  if (docScroll >= menuOffsetTop) {
+      $menu.addClass('fixed');
+  } else {
+      $menu.removeClass('fixed');
+  }
 });
