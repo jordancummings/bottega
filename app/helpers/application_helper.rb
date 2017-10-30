a8944ded1302485d6ac170f1d4416c338904fbf0
Mac <mac@MacBook-Pro.ad.uvu.edu>
2017-10-30 12:17:20 -0600

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
@@ -19,6 +18,46 @@ module ApplicationHelper
 
   def copyright_generator
     CummingsViewTool::Renderer.copyright "Jordan Cummings", "All rights (and lefts) reserved"
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+            {
+        url: about_path,
+        title: "About Me"
+      },
+            {
+        url: contact_path,
+        title: "Contact"
+      },
+            {
+        url: blogs_path,
+        title: "Blog"
+      },
+            {
+        url: portfolios_path,
+        title: "Porftolio"
+      },
+
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
   end
 
 end
